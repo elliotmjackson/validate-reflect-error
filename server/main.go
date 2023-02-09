5fe94c20e8672e8664651825f10ff0f77164a7cf
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-02-09 17:20:04 -0500

    use validation

--- server/main.go
+++ server/main.go
@@ -21,6 +21,9 @@ func (s *GreetServer) Greet(
 	_ context.Context,
 	req *connect.Request[greetv1.GreetRequest],
 ) (*connect.Response[greetv1.GreetResponse], error) {
+	if err := req.Msg.ValidateAll(); err != nil {
+		return nil, err
+	}
 	log.Println("Request headers: ", req.Header())
 	res := connect.NewResponse(&greetv1.GreetResponse{
 		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
